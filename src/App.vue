f7aa849ca43c6dd6e54e85b83012fd110399e354
Matt <matheusc039@hotmail.com>
2023-06-05 16:59:42 -0300

    setting up router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-    <h1 class="font-extrabold text-4xl text-purple-600">Autopass A3</h1>
+    <RouterView/>
 </template>
